36234d46207e17753c88d668d84204eff306d762
Ludwig Stecher <ludwig.stecher@gmx.de>
2022-01-03 17:19:54 +0100

    Set port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const Uwuifier = require('uwuifier')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 80;
 
 const uwuifier = new Uwuifier();
 
