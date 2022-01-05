32442dfd90e6e0c3237ce23d1e1495c8ca2f621a
Ludwig Stecher <ludwig.stecher@gmx.de>
2022-01-05 09:53:56 +0100

    Show line breaks in output

--- src/public/script.js
+++ src/public/script.js
@@ -2,7 +2,7 @@ async function send() {
     const text = document.getElementById('input').value;
     const result = await fetch('/uwu?text=' + encodeURIComponent(text));
     const resultText = await result.text();
-    document.getElementById('result').textContent = resultText;
+    document.getElementById('result').innerText = resultText;
 }
 
 document.getElementById('submit').addEventListener("click", send);
